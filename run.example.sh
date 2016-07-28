f577a8f735969945d5864e14c432abf99a5f0d3f
David Tsai <davidt@syapse.com>
2016-07-27 23:10:48 -0700

    Adding example shell file to be user modified

---
+++ run.example.sh
@@ -0,0 +1,8 @@
+# In order to use this file, you'll need to copy this and name it "somefilename.sh":
+# `$ cp run.example.sh somefilename.sh`
+# 
+# Replace: auth, user login info, and location with your config
+#
+# To run: `$ sh somefilename.sh`
+
+python ./pogo/demo.py -a google -u user@example.com -p userpassword -l "lat, lon"
